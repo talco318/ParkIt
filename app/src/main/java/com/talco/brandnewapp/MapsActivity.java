f2a70f6823811090d8dde9f104a7948f0e1f4a89
Tal Cohen <Tal.Cohen2@s.afeka.ac.il>
2022-06-29 20:23:42 +0300

    initial commit "with write func to database in onMapReady!!!"

--- app/src/main/java/com/talco/brandnewapp/MapsActivity.java
+++ app/src/main/java/com/talco/brandnewapp/MapsActivity.java
@@ -111,18 +111,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         Intent i = getIntent();
         Location location = (Location) i.getParcelableExtra("key");
-
-
         LatLng currentLoc = new LatLng(Double.parseDouble(location.get_Latitude()), Double.parseDouble(location.get_Longitude()));
         LatLng sec = new LatLng(32.073698, 34.781924); // this is a test
-
         mMap.addMarker(new MarkerOptions().position(currentLoc).title("Your current location"));
-
         mMap.addMarker(new MarkerOptions().position(sec).title("Your sec location"));
-
         mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLoc));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
