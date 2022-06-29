b5afbdbd99042eb4189bd2666a6e30672daeece6
Tal Cohen <Tal.Cohen2@s.afeka.ac.il>
2022-06-29 13:57:44 +0300

    initial commit

--- app/src/main/java/com/talco/brandnewapp/MainActivity.java
+++ app/src/main/java/com/talco/brandnewapp/MainActivity.java
@@ -3,13 +3,11 @@ package com.talco.brandnewapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
