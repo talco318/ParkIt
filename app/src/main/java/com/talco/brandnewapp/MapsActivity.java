70aa8f5f3559e3e810ea1884c77940dd1b4c0d04
Tal Cohen <Tal.Cohen2@s.afeka.ac.il>
2022-07-01 17:17:32 +0300

    initial commit "read locations number "

--- app/src/main/java/com/talco/brandnewapp/MapsActivity.java
+++ app/src/main/java/com/talco/brandnewapp/MapsActivity.java
@@ -28,8 +28,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.talco.brandnewapp.databinding.ActivityMapsBinding;
 
 import java.io.IOException;
@@ -44,7 +47,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationManager locationManager;
     private String provider;
     private FirebaseAuth mAuth;
-
+    public int locNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,9 +111,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Intent i = getIntent();
         Location location = (Location) i.getParcelableExtra("key");
         LatLng currentLoc = new LatLng(Double.parseDouble(location.get_Latitude()), Double.parseDouble(location.get_Longitude()));
-        LatLng sec = new LatLng(32.073698, 34.781924); // this is a test
+        //LatLng sec = new LatLng(32.073698, 34.781924); // this is a test
         mMap.addMarker(new MarkerOptions().position(currentLoc).title("Your current location"));
-        mMap.addMarker(new MarkerOptions().position(sec).title("Your sec location"));
+        //mMap.addMarker(new MarkerOptions().position(sec).title("Your sec location"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLoc));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
@@ -124,12 +127,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //addLocationsToMap(mMap);
+        addLocationsToMap(mMap);
         mMap.setMyLocationEnabled(true);
 
     }
 
     public void addLocationsToMap(GoogleMap googleMap) {
+        //hard codded:
+        LatLng sec = new LatLng(32.073698, 34.781924); // this is a test
+        LatLng third = new LatLng(32.095280, 34.871420); // this is a test
+        mMap.addMarker(new MarkerOptions().position(sec).title("Your sec location"));
+        mMap.addMarker(new MarkerOptions().position(third).title("Your third location"));
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference locNumberRef = database.getReference("Location number");
+
+        DatabaseReference locationsRef = database.getReference("Locations");
+
+        locNumberRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                locNumber = (int)snapshot.getValue(int.class);
+                for (int i=0; i<locNumber; i++){
+                    //Log.d("result" , "i is "+ i);
+                    Log.d("result" , "the number of locs is "+ locNumber + " i is " + i );
+
+                    //Location loc = (Location) snapshot.getValue(Location.class);
+                    //LatLng locLat = new LatLng(Double.parseDouble(loc.get_Latitude()), Double.parseDouble(loc.get_Longitude()));
+                    //Log.d("result" , " " + i + "location is "+ loc.toString());
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
     }
@@ -176,12 +211,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Location l = new Location(lat, longlat);
         DatabaseReference myRef = database.getReference("User").child("User id: " + id).child("Location id: " + l.get_id());
         DatabaseReference publicRef = database.getReference("Locations").child("Location id: " + l.get_id());
+        DatabaseReference locNumberRef = database.getReference("Location number");
 
         Log.d("result:", myRef.toString());
         Log.d("result:", publicRef.toString());
 
         publicRef.setValue(l);
         myRef.setValue(l);
+        locNumberRef.setValue(l.get_id());
         //addToView();
         Toast.makeText(this, "Location added!", Toast.LENGTH_LONG).show();
 
