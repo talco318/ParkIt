4e6c0117a0ac86f2641483d283f4f7d78f8483c2
Tal Cohen <Tal.Cohen2@s.afeka.ac.il>
2022-07-01 16:35:56 +0300

    initial commit "getfull func is better"

--- app/src/main/java/com/talco/brandnewapp/MapsActivity.java
+++ app/src/main/java/com/talco/brandnewapp/MapsActivity.java
@@ -98,17 +98,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Geocoder geocoder;
         List<Address> addresses;
         geocoder = new Geocoder(this, Locale.getDefault());
-
         addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
-
         String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
         String city = addresses.get(0).getLocality();
-        //String state = addresses.get(0).getAdminArea();
-        //String country = addresses.get(0).getCountryName();
-        //String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
-
         LocationField.setText(address+" "+ city  + " " );
-
     }
 
 
